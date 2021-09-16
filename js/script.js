e5d6a38360e7f7d74a73c77c11a254bc997a79af
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-16 16:18:47 +0200

    Update exercise

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ new Vue ({
     el: "#divVue",
     data: {
         title: "Forza Milan",
-        imgSrc: "https://www.fansmagazine.eu/wp-content/uploads/2020/03/Curva-Sud-Milano.jpg"
+        imgSrc: "https://www.fansmagazine.eu/wp-content/uploads/2020/03/Curva-Sud-Milano.jpg",
+        imgAlt: "Curva Sud Milano"
     }
 })
